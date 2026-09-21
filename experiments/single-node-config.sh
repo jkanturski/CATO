67bdc8ae1d337ab0965b4bb85df92b47cc5a5a21
jkanturski <54432291+jkanturski@users.noreply.github.com>
2026-09-21 16:47:57 +0200

    Delete experiments/single-node-config.sh

--- experiments/single-node-config.sh
+++
@@ -1,8 +0,0 @@
-#BSUB -J train_2gpu_1node
-#BSUB -n 2
-#BSUB -R "span[ptile=2]"
-#BSUB -gpu "num=2:mode=exclusive_process"
-#BSUB -q gpu
-
-# Added --model flag and updated script name
-torchrun --standalone --nnodes=1 --nproc_per_node=2 train_lstm_tcn_ddp.py --model lstm --horizon 1
